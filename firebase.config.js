4533061ad210f306eb9bdfe4493270e1e360e778
cantor11 <junior.cantor@correounivalle.edu.co>
2024-10-22 12:58:29 -0500

    Login interface added with documentation

--- firebase.config.js
+++ firebase.config.js
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Web app's Firebase configuration
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
     authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import RoutesProject from './routes/RoutesProject'
 
+/**
+ * Entry point for the React application. 
+ * This file renders the main application component, 
+ * encapsulated in StrictMode for highlighting potential problems 
+ * in the application. It sets up the routing using RoutesProject.
+ */
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <RoutesProject />
