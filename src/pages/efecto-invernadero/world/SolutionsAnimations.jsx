8464152642152194b7463c2fd4979e21f40c2c67
JuanC513U <juan.esteban.clavijo@correounivalle.edu.co>
2024-11-23 21:51:32 -0500

    adding solution 1 scene

--- src/pages/efecto-invernadero/world/SolutionsAnimations.jsx
+++ src/pages/efecto-invernadero/world/SolutionsAnimations.jsx
@@ -7,12 +7,14 @@ import NormalGasAnimation from "./NormalGasAnimation";
 import SolutionsStage from "./SolutionsStage";
 import useGreeenhouseStore from "../../../stores/greenhouse-store";
 
+import Solution1 from "./Solution1";
+
 /**
  * SolutionsAnimations component
  * 
  * This functional React component sets up animations using other components based
  * on the current line being seen in solutions section.
- * We have: normal amount of Greenhouse gases over Earth.
+ * We have: normal amount of Greenhouse gases over Earth, Solution 1.
  * To know the current line it will get the information brought from a store made with Zustand.
  * In addition, show some objets with Physics, colliders and Forces for the environment of the page.
  */
@@ -31,8 +33,13 @@ const SolutionsAnimations = () => {
         <DodecahedronContainer position={DodecaedronBallPosition} />
 
         <SolutionsStage />
+
+        {solutionsSection.solutionsStep == 1 ?
+          <Solution1 />
+          : null}
         {/* <SocialMediaSetModel scale={15} position={[-550,300,-230]} rotation-y={Math.PI*1/2} /> */}
       </Physics>
+
       {solutionsSection.solutionsStep >= 0 ?
         <NormalGasAnimation isAnimating={true} />
         : null}
