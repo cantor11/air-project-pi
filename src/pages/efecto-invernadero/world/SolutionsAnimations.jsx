cb0ea14e238ff640a2faf8191ecac5ec6f1d6c6a
JuanC513U <juan.esteban.clavijo@correounivalle.edu.co>
2024-11-24 18:43:29 -0500

    adding solution 5 scene

--- src/pages/efecto-invernadero/world/SolutionsAnimations.jsx
+++ src/pages/efecto-invernadero/world/SolutionsAnimations.jsx
@@ -11,13 +11,14 @@ import Solution1 from "./Solution1";
 import Solution2 from "./Solution2";
 import Solution3 from "./Solution3";
 import Solution4 from "./Solution4";
+import Solution5 from "./Solution5";
 
 /**
  * SolutionsAnimations component
  * 
  * This functional React component sets up animations using other components based
  * on the current line being seen in solutions section.
- * We have: normal amount of Greenhouse gases over Earth, Solution 1, Solution 2, Solution 3, solution 4.
+ * We have: normal amount of Greenhouse gases over Earth, Solution 1, Solution 2, Solution 3, solution 4, solution 5.
  * To know the current line it will get the information brought from a store made with Zustand.
  * In addition, show some objets with Physics, colliders and Forces for the environment of the page.
  */
@@ -48,6 +49,9 @@ const SolutionsAnimations = () => {
           : null}
         {solutionsSection.solutionsStep === 4 ?
           <Solution4 />
+          : null}
+        {solutionsSection.solutionsStep === 5 ?
+          <Solution5 />
           : null}
         {/* <SocialMediaSetModel scale={15} position={[-550,300,-230]} rotation-y={Math.PI*1/2} /> */}
       </Physics>
