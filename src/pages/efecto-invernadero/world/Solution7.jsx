917669c64ec77995433449fbc62f914a17b206ba
JuanC513U <juan.esteban.clavijo@correounivalle.edu.co>
2024-11-24 22:09:13 -0500

    adding solution 7 scene

---
+++ src/pages/efecto-invernadero/world/Solution7.jsx
@@ -0,0 +1,47 @@
+import { RigidBody } from "@react-three/rapier";
+
+import { BucketModel } from "./BucketModel";
+import { CloudModel } from "./CloudModel";
+import { PurpleRainstormModel } from "./PurpleRainstormModel";
+
+/**
+ * Solution7 Component
+ * 
+ * This functional React component sets up sme 3D objects imported in this proyect
+ * to use them to show an animation. The animation simulates some objects like
+ * buckets, clouds and rain which will be used for the immersion in the page.
+ * Some objects are put inside rigid bodies to put them in physics environment.
+ */
+
+
+const Solution7 = () => {
+
+  return (
+    <>
+      <RigidBody
+        name="bucket1"
+        restitution={2}
+        type="dynamic"
+        gravityScale={9}
+        colliders="hull"
+      >
+        <BucketModel position={[-800,300,-150]} scale={2000} />
+      </RigidBody>
+      <RigidBody
+        name="bucket2"
+        restitution={2}
+        type="dynamic"
+        gravityScale={9}
+        colliders="hull"
+      >
+        <BucketModel position={[-800,300,-350]} scale={2000} />
+      </RigidBody>
+      <CloudModel position={[-850,400,-120]} scale={20} />
+      <CloudModel position={[-850,400,-260]} scale={20} />
+      <CloudModel position={[-850,400,-400]} scale={20} />
+      <PurpleRainstormModel position={[-850,0,-250]} scale={2} />
+    </>
+  )
+}
+
+export default Solution7;
--- src/pages/efecto-invernadero/world/SolutionsAnimations.jsx
+++ src/pages/efecto-invernadero/world/SolutionsAnimations.jsx
@@ -13,13 +13,14 @@ import Solution3 from "./Solution3";
 import Solution4 from "./Solution4";
 import Solution5 from "./Solution5";
 import Solution6 from "./Solution6";
+import Solution7 from "./Solution7";
 
 /**
  * SolutionsAnimations component
  * 
  * This functional React component sets up animations using other components based
  * on the current line being seen in solutions section.
- * We have: normal amount of Greenhouse gases over Earth, Solution 1, Solution 2, Solution 3, solution 4, solution 5, solution 6.
+ * We have: normal amount of Greenhouse gases over Earth, Solution 1, Solution 2, Solution 3, solution 4, solution 5, solution 6, solution 7.
  * To know the current line it will get the information brought from a store made with Zustand.
  * In addition, show some objets with Physics, colliders and Forces for the environment of the page.
  */
@@ -58,10 +59,13 @@ const SolutionsAnimations = () => {
         {solutionsSection.solutionsStep === 6 ?
           <Solution6 />
           : null}
+        {solutionsSection.solutionsStep === 7 ?
+          <Solution7 />
+          : null}
         {/* <SocialMediaSetModel scale={15} position={[-550,300,-230]} rotation-y={Math.PI*1/2} /> */}
       </Physics>
 
-      {solutionsSection.solutionsStep >= 0 ?
+      {solutionsSection.solutionsStep >= 90 ?
         <NormalGasAnimation isAnimating={true} />
         : null}
     </>
