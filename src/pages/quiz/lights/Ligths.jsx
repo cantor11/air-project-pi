c7246133c5557f278708b2241ae40d6fc3b87719
XxCr1ss <cabrerapantojacristiandavid@gmail.com>
2024-12-08 00:29:27 -0500

    Base added for quiz section

---
+++ src/pages/quiz/lights/Ligths.jsx
@@ -0,0 +1,16 @@
+
+/**
+ * Lights component defines the lighting setup for the 3D scene.
+ * It adds spot light to the scene to provide illumination focusing on a target.
+ */
+
+const Lights = () => {
+
+  return (
+    <>
+      <ambientLight />
+    </>
+  )
+};
+
+export default Lights;
