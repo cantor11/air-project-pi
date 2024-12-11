125062d5ea7690fbc64fde501501c6e241045b93
XxCr1ss <cabrerapantojacristiandavid@gmail.com>
2024-12-10 22:09:17 -0500

    Added logic to sync firebase data for quiz

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,12 @@ import "./Header.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useCallback } from "react";
 import useAuthStore from "../../stores/use-auth-store";
+import useQuizStore from "../../stores/quiz-store";
 import { NavLink } from "react-router-dom";
+
+import { db } from "../../../../firebase.config"; // Ajusta la ruta según tu estructura
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { auth } from "../../../../firebase.config";
 
 /**
  * Header Component
@@ -16,12 +21,40 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   const { user, logout } = useAuthStore();
+  const { questionsSection, setQuestionsSection } = useQuizStore(); // Información del store
   const navigate = useNavigate();
 
   const handleLogout = useCallback(() => {
     logout();
     navigate("/login");
   }, [logout]);
+
+  const syncFirestoreData = async () => {
+    if (!auth.currentUser) {
+      alert("Debes iniciar sesión para enviar el quiz.");
+      return;
+    }
+
+    const userId = auth.currentUser.uid;
+    const userRef = doc(db, "quizzes", userId);
+
+    try {
+      const userSnapshot = await getDoc(userRef);
+      const existingData = userSnapshot.exists() ? userSnapshot.data() : null;
+
+      const oldScoreSum = existingData?.userScore?.reduce((a, b) => a + b, 0) || 0;
+      if (oldScoreSum != 0) {
+        setQuestionsSection({
+          bestUserScore : existingData.userScore
+        });
+        console.log(questionsSection.bestUserScore) 
+      } 
+    } catch (error) {
+      console.error("Error al guardar el quiz en DB:", error);
+      alert("Ocurrió un error al enviar el quiz. Revisar consola.");
+    }
+  };
+
   return (
     <header>
       <div className="left-section">
@@ -59,6 +92,7 @@ const Header = () => {
             <NavLink
               to="/quiz"
               className={({ isActive }) => isActive ? "button-link active" : "button-link"}
+              onClick={syncFirestoreData}
             >
               Quiz
             </NavLink>
